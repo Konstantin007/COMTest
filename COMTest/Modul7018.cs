1243aafa25e0aeb5de381bf70232e719ed1d5915
Arslan <arslan92@mail.ru>
2012-10-18 17:48:05 +0400

    Залита программа

---
+++ COMTest/Modul7018.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace COMTest
+{
+    class Modul7018
+    {
+        private SerialPort port;
+ 
+        public Modul7018(String portName)
+        {
+            this.openPort(portName);
+        }
+
+        private void openPort(String portName)
+        {
+            port = new SerialPort();
+            port.PortName = portName;
+            port.BaudRate = 9600;
+            port.DataBits = 8;
+            port.StopBits = StopBits.One;
+            port.Parity = Parity.None;
+            port.Handshake = Handshake.None;
+            port.ReceivedBytesThreshold = 8;
+            port.WriteBufferSize = 20;
+            port.ReadBufferSize = 20;
+            port.ReadTimeout = -1;
+            port.WriteTimeout = -1;
+            port.DtrEnable = false;
+            port.Open();
+            port.RtsEnable = true;
+            System.Threading.Thread.Sleep(100);
+        }
+
+        public void writeToPort(String command)
+        {
+            port.WriteLine(command + (char)0x0D);
+        }
+
+        public String readFromPort()
+        {
+            System.Threading.Thread.Sleep(100);
+            byte[] data = new byte[port.BytesToRead];
+            port.Read(data, 0, data.Length);
+            port.DiscardInBuffer();
+            String str = "";
+            for (int i = 0; i < data.Length; i++)
+            {
+                str += ((char)data[i]).ToString();
+            }
+
+            str = "\t" + str;
+
+            return str;
+        }
+
+        public void closePort()
+        {
+            if (port.IsOpen)
+            {
+                port.DiscardOutBuffer();
+                port.DiscardInBuffer();
+                port.Close();
+            }
+        }
+    }
+}
---
+++ COMTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace COMTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Modul7018 modul = new Modul7018("COM7");
+
+            Console.WriteLine("Port is opened");
+            Random r = null;
+            int x = 0;
+
+            int i = 1;
+            do
+            {
+                r = new Random();
+                x = r.Next(99);
+                                
+                modul.writeToPort("@02"+x);
+                Console.WriteLine(modul.readFromPort());
+
+                modul.writeToPort("@02");
+                Console.WriteLine(modul.readFromPort());
+
+                i++;
+            } while (i < 2);
+
+            
+            
+            modul.closePort();
+            Console.WriteLine("Port is closed");
+            Console.ReadKey();
+        }
+    }
+}
